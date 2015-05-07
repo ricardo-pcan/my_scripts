e1809d58e489b64d521b5c086e55b55b892d9316
ripper <ricardo.pcan@gmail.com>
2015-05-07 18:57:59 -0500

    add zsh file

---
+++ main/zsh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+function zsh(){
+	echo ""
+	echo "###############################################"
+	echo "############### INSTALL < ZSH > ###############"
+	echo "###############################################"
+	echo ""
+	sleep 2;
+	(
+		sudo apt-get install zsh -y
+		curl -L http://install.ohmyz.sh | sh 
+	) &> /dev/null && echo -e "$green OK $endcolor" || echo -e "$red FAILED $endcolor";
+	
+}
