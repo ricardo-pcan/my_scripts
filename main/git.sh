69d406f2132c512fe85917c8a15779ca5f5482c6
Ricardo <ripper@ripper.lenovo>
2015-05-10 00:08:13 -0500

    add git config, zsh

--- main/git.sh
+++ main/git.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-function git() {
+function gitInstall() {
 	echo ""
 	echo "###########################################"
 	echo "############### INSTALL GIT ###############"
@@ -8,6 +8,8 @@ function git() {
 	sleep 2
 	(
 		sudo apt-get install git -y
+		git config --global user.name $USER
+		git config --global user.email ricardo.pcan@gmail.com
 	) &> /dev/null && echo -e "$green OK $endcolor" || echo -e "$red FAILED $endcolor";
 	
 }
--- ripper.sh
+++ ripper.sh
@@ -20,7 +20,7 @@
 
 			# MAIN	
 
-				git
+				gitInstall
 				svn
 				curl
 				terminator
