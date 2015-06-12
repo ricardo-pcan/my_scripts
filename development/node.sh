df8d10eb4c9cb5358c5fe84b560738b95fbab021
ripper <ricardo.pcan@gmail.com>
2015-06-12 09:06:32 -0500

    add node

--- development/node.sh
+++ development/node.sh
@@ -7,8 +7,10 @@ function node(){
 	echo ""
 	sleep 2;
 	(
-		sudo apt-get install nodejs nodejs-dev npm -y
-		
+		git clone git://github.com/creationix/nvm.git ~/.nvm
+		printf "\n\n# NVM\nif [ -s ~/.nvm/nvm.sh ]; then\n\tNVM_DIR=~/.nvm\n\tsource ~/.nvm/nvm.sh\nfi" >> ~/.bashrc
+		NVM_DIR=~/.nvm
+		source ~/.nvm/nvm.sh
 	) &> /dev/null && echo -e "$green OK $endcolor" || echo -e "$red FAILED $endcolor";
 	
 }
